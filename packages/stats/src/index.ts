f8f2e7c0a467c7a84b8842d6770011baef5b77bf
Halit Ozger <haltozger02@gmail.com>
2026-06-12 14:03:28 +0300

    feat(stats): export audit surface; pin allow-list

--- packages/stats/src/index.ts
+++ packages/stats/src/index.ts
@@ -11,3 +11,28 @@ export {
 } from "./store.js";
 
 export { StatsError, statsErrorCodeSchema, type StatsErrorCode } from "./errors.js";
+
+export {
+  auditEventSchema,
+  type AuditEvent,
+  contextPackBuiltEventSchema,
+  ruleAppliedEventSchema,
+  failureAvoidedEventSchema,
+  memoryRetrievedEventSchema,
+  toolRouteEventSchema,
+} from "./audit-event.js";
+
+export {
+  auditSummarySchema,
+  type AuditSummary,
+  auditWindowSchema,
+  type AuditWindow,
+  summarizeAudit,
+  type SummarizeAuditOptions,
+} from "./audit-summary.js";
+
+export {
+  appendAuditEvent,
+  type AppendAuditEventInput,
+  readAuditEvents,
+} from "./audit-store.js";
