b4cfaa8d475e51b13bf55ca3718f641ed9a7ca23
Halit Ozger <haltozger02@gmail.com>
2026-07-20 00:03:11 +0300

    feat(memory): define long memory contracts

--- packages/long-memory/src/index.ts
+++ packages/long-memory/src/index.ts
@@ -1,1 +1,15 @@
 export const LONG_MEMORY_PACKAGE = "@megasaver/long-memory";
+export {
+  observationKindSchema,
+  observationSchema,
+  recallRequestSchema,
+  recallItemSchema,
+  receiptItemSchema,
+  recallBundleSchema,
+  type ObservationKind,
+  type Observation,
+  type RecallRequest,
+  type RecallItem,
+  type ReceiptItem,
+  type RecallBundle,
+} from "./model.js";
