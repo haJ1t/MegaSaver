8ed418c9361aaf2fb08e7b02cc22358b314aa3c8
Halit Ozger <haltozger02@gmail.com>
2026-07-26 17:51:25 +0300

    feat(memory): use cached offline embeddings

--- packages/embeddings/src/index.ts
+++ packages/embeddings/src/index.ts
@@ -1,3 +1,3 @@
 export { cosine } from "./cosine.js";
-export { embed } from "./embed.js";
+export { embed, embedOffline } from "./embed.js";
 export { readVectorIds, readVectors, writeVectors, type VectorEntry } from "./store.js";
