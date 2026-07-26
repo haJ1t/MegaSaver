3d1a7d89dc19f2f479d6b020b7d187e4ef10547d
Halit Ozger <haltozger02@gmail.com>
2026-07-26 17:50:56 +0300

    fix(memory): retain indexed recall candidates

--- packages/embeddings/src/index.ts
+++ packages/embeddings/src/index.ts
@@ -1,3 +1,3 @@
 export { cosine } from "./cosine.js";
 export { embed } from "./embed.js";
-export { readVectors, writeVectors, type VectorEntry } from "./store.js";
+export { readVectorIds, readVectors, writeVectors, type VectorEntry } from "./store.js";
